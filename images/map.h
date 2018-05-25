fc79479dc051b05da5d1ce77aa09f149142266cf
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2018-05-25 03:54:09 +0300

    Add files via upload
    Added limited time;
    Added limited ammos;
    Added pause;
    Added pplayerScore changing;
    Added level1Endings;

---
+++ images/map.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+const int HEIGHT_MAP = 5000;// 25;
+const int WIDTH_MAP = 10000;//40;
+const int BLOCK_SIZE = 1000;
+const int RAD_SIZE = 180;
